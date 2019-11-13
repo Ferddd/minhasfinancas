c6455bc7a34fcba504a2b5be559bb82ec210bba1
ferd <fernandohoflinger@gmail.com>
2019-11-13 12:39:25 -0300

    pequenas refatoracoes

--- src/main/java/app/com/minhasfinancas/api/controller/LancamentoController.java
+++ src/main/java/app/com/minhasfinancas/api/controller/LancamentoController.java
@@ -1,12 +1,18 @@
 package app.com.minhasfinancas.api.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.com.minhasfinancas.api.dto.LancamentoDto;
@@ -19,61 +25,99 @@ import app.com.minhasfinancas.service.LancamentoService;
 import app.com.minhasfinancas.service.UsuarioService;
 
 @RestController
-@RequestMapping("/api/usuarios")
+@RequestMapping("/api/lancamentos")
 public class LancamentoController {
 
 	private LancamentoService service;
 	private UsuarioService usuario;
-	
-	public LancamentoController (LancamentoService service) {
+
+	//para não precisar injetar dependencias no construtor, pode-se utilizar @RequiredArgsConstrucor e declarar atributos como final 
+	public LancamentoController(LancamentoService service, UsuarioService usuario) {
 		this.service = service;
+		this.usuario = usuario;
 	}
-	
+
+	@GetMapping
+	public ResponseEntity buscar(@RequestParam(value = "descricao", required = false) String descricao,
+			@RequestParam(value = "mes", required = false) Integer mes,
+			@RequestParam(value = "ano", required = false) Integer ano, @RequestParam("usuario") Long idUsuario) {
+
+		Lancamento lancamentoFiltro = new Lancamento();
+		lancamentoFiltro.setDescricao(descricao);
+		lancamentoFiltro.setMes(mes);
+		lancamentoFiltro.setAno(ano);
+
+		Optional<Usuario> user = usuario.obterPorId(idUsuario);
+		if (!user.isPresent()) {
+			return ResponseEntity.badRequest().body("Usuário não encontrado");
+		} else {
+			lancamentoFiltro.setUsuario(user.get());
+		}
+
+		List<Lancamento> lancamentos = service.buscar(lancamentoFiltro);
+		return ResponseEntity.ok(lancamentos);
+	}
+
 	@PostMapping
-	public ResponseEntity salvar (@RequestBody LancamentoDto dto) {
-		
+	public ResponseEntity salvar(@RequestBody LancamentoDto dto) {
+
 		try {
 			Lancamento entidade = converter(dto);
 			entidade = service.salvar(entidade);
-		
+
 			return new ResponseEntity(entidade, HttpStatus.CREATED);
-		}catch (RegraNegocioException e) {
+		} catch (RegraNegocioException e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
 	}
-	
+
 	@PutMapping("{id}")
-	public ResponseEntity atualizar (@PathVariable("id")  Long id, @RequestBody LancamentoDto dto ) {
+	public ResponseEntity atualizar(@PathVariable("id") Long id, @RequestBody LancamentoDto dto) {
 		return service.obterPorId(id).map(entity -> {
-											try {
-												Lancamento lancamento = converter(dto); 
-												lancamento.setId(entity.getId());
-												service.atualizar(lancamento);
-												return ResponseEntity.ok(lancamento);
-											}catch (RegraNegocioException e) {
-												return ResponseEntity.badRequest().body(e.getMessage());
-											}
-												})
-												.orElseGet( ()-> new ResponseEntity("Lancamento nao encontrado", HttpStatus.BAD_REQUEST));		
-	
+			try {
+				Lancamento lancamento = converter(dto);
+				lancamento.setId(entity.getId());
+				service.atualizar(lancamento);
+				return ResponseEntity.ok(lancamento);
+			} catch (RegraNegocioException e) {
+				return ResponseEntity.badRequest().body(e.getMessage());
+			}
+		}).orElseGet(() -> new ResponseEntity("Lancamento nao encontrado", HttpStatus.BAD_REQUEST));
+
 	}
-	
-	private Lancamento converter (LancamentoDto dto) {
+
+	@DeleteMapping("{id}")
+	public ResponseEntity deletar(@PathVariable("id") Long id) {
+
+		return service.obterPorId(id).map(entidade -> {
+			service.deletar(entidade);
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
+		}).orElseGet(() -> new ResponseEntity("Lancamento nao encontrado", HttpStatus.BAD_REQUEST));
+
+	}
+
+	private Lancamento converter(LancamentoDto dto) {
 		Lancamento lancamento = new Lancamento();
-		
+
 		lancamento.setId(dto.getId());
 		lancamento.setDescricao(dto.getDescricao());
 		lancamento.setMes(dto.getMes());
 		lancamento.setAno(dto.getAno());
 		lancamento.setValor(dto.getValor());
-		
-		Usuario user =usuario.obterPorId(dto.getUsuario()).orElseThrow(() -> new RegraNegocioException("Usuário não econtrado"));
-		
+
+		Usuario user = usuario.obterPorId(dto.getUsuario())
+				.orElseThrow(() -> new RegraNegocioException("Usuário não econtrado"));
+
 		lancamento.setUsuario(user);
+		
+		if(dto.getTipo() != null) {
 		lancamento.setTipo(TipoLancamento.valueOf(dto.getTipo()));
+		}
+		
+		if(dto.getStatus() != null) {
 		lancamento.setStatus(StatusLancamento.valueOf(dto.getStatus()));
-		
+		}
 		return lancamento;
-		
+
 	}
 }
